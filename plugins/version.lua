14587d33b5a81e711808961fdfdd0d70b7058838
BooM-amour <lov3_4_3v3r_666@yahoo.com>
2016-03-01 06:44:44 -0800

    Update version.lua

--- plugins/version.lua
+++ plugins/version.lua
@@ -6,16 +6,16 @@
 --                                              --
 --------------------------------------------------
 --                                              --
---       Developers: @Josepdal & @MaSkAoS       --
---     Support: @Skneos,  @iicc1 & @serx666     --
+--       Developers:  @vvViRuSss      --
+--     Support: @vvViRuSss     --
 --                                              --
 --------------------------------------------------
 
 do
 
 function run(msg, matches)
-  return 'DBTeam Bot V1 Supergroups\nAn advanced Administration bot based on yagop/telegram-bot \n\nhttp://github.com/Josepdal/DBTeam\n\nDevelopers :\n@MaSkAoS\n@Josepdal\n\nSupport :\n@skneos\n@iicc1\n@Serx666' 
-  --[[ Checkout https://github.com/Josepdal/DBTeam
+  return 'DBTeam Bot V1 Supergroups\nAn advanced Administration bot based on yagop/telegram-bot \n\nhttp://github.com/Josepdal/DBTeam\n\nDevelopers :\n@vvViRuSss\n@brunitim\n\nSupport :\n@vvViRuSss
+  --[[ Checkout https://github.com/BooM-amour/supergp
   --GNU GPL v2 license.]]
 end
 
